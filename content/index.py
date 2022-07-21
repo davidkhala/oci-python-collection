ac54b199a9797bfb24b857dc0e14e62864647c27
David Liu <david.yx.liu@oracle.com>
2022-07-21 12:10:55 +0800

    no pdm on windows

--- content/index.py
+++ content/index.py
@@ -1,6 +1,7 @@
 # /documents/api/1.2/folders/{folderId}
 from http_request import get
 
+
 class Base:
     tenancy_name: str
     instance_name: str
@@ -12,6 +13,7 @@ class Base:
     def url(self):
         return "{}-{}.cec.ocp.oraclecloud.com".format(self.instance_name, self.tenancy_name)
 
-    def login(self):
+
+    def login(self, tokenValue):
+        url = 'https://cx-hktwlab.cec.ocp.oraclecloud.com/documents/api/1.2/folders/items'
         return get('https://cx-hktwlab.cec.ocp.oraclecloud.com/documents/web?IdcService=GET_OAUTH_TOKEN')
-        
